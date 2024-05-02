be3bd6296b9c2bbf6fe9a34fb04d976f7eedbeda
MrGarlic1 <61215937+MrGarlic1@users.noreply.github.com>
2024-05-02 19:30:26 +0100

    Add files via upload
    add automatic path detection for .env

--- dist/linux_x86_64/autorun.sh
+++ dist/linux_x86_64/autorun.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-cd "$(dirname "$0")"
+SOURCE_DIR="$(dirname "$0")"
+cd "$SOURCE_DIR"
 
+if ! grep -q "$SOURCE_DIR" .env; then
+    sed -i '/PARENT=/d' ./.env
+    echo "PARENT="${SOURCE_DIR}"" >> .env
+fi
 if [ ! -d ./venv/bin ]; then
     python3 -m venv venv
     source ./venv/bin/activate
     pip install -r requirements.txt
 fi
+
 source ./venv/bin/activate
 python main.py
