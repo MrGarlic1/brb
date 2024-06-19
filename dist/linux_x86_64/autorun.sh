c39384a35f243874d7be22f0d26e3aa23a222667
Garlic <garlic@mrgarlic.duckdns.org>
2024-06-19 05:53:36 +0000

    fix: update autorun dir for venv install

--- dist/linux_x86_64/autorun.sh
+++ dist/linux_x86_64/autorun.sh
@@ -3,11 +3,11 @@ SOURCE_DIR="$(dirname "$0")"
 SOURCE_DIR="$(readlink -e $SOURCE_DIR/../../src)"
 cd "$SOURCE_DIR"
 
-if [ ! -f ./venv/bin/activate ]; then
+if [ ! -f ./../venv/bin/activate ]; then
     python3 -m venv venv
-    source ./venv/bin/activate
+    source ./../venv/bin/activate
     pip install -r ./../requirements.txt
 fi
 
-source ./venv/bin/activate
+source ./../venv/bin/activate
 python main.py
