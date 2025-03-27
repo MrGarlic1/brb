26af9d17aa65a803c629aa5bb966e937cbb52cee
MrGarlic1 <ben@samans.com>
2025-03-26 20:29:54 -0400

    fix: ensure using correct pip version for autorun

--- dist/linux_x86_64/autorun.sh
+++ dist/linux_x86_64/autorun.sh
@@ -4,7 +4,7 @@ SOURCE_DIR="$(readlink -e $SOURCE_DIR/../../src)"
 cd "$SOURCE_DIR"
 
 if [ ! -f ./../.venv/bin/activate ]; then
-    python3.13 -m venv .venv
+    python3.13 -m venv ./../.venv
     source ./../.venv/bin/activate
     python3.13 -m pip install -r ./../requirements.txt
 fi
