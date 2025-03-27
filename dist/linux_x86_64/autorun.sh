5b2f40b143cd5ce33dd2d057f6475458dfc4ad06
MrGarlic1 <ben@samans.com>
2025-03-26 20:14:13 -0400

    chore: bump python version to 3.13 in linux autorun

--- dist/linux_x86_64/autorun.sh
+++ dist/linux_x86_64/autorun.sh
@@ -4,8 +4,8 @@ SOURCE_DIR="$(readlink -e $SOURCE_DIR/../../src)"
 cd "$SOURCE_DIR"
 
 if [ ! -f ./../venv/bin/activate ]; then
-    python3 -m venv venv
-    source ./../venv/bin/activate
+    python3.13 -m venv .venv
+    source ./../.venv/bin/activate
     pip install -r ./../requirements.txt
 fi
 
