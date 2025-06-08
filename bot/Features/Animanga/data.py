570cb9396d9616feac9b4b19158c1705de05ce7f
MrGarlic1 <ben@samans.com>
2025-06-07 20:41:57 -0400

    refactor: optimizations and readability

--- bot/Features/Animanga/data.py
+++ bot/Features/Animanga/data.py
@@ -1,7 +1,7 @@
-from asyncio import gather
+from asyncio import gather, sleep, Semaphore
 from datetime import datetime
 from random import uniform
-from typing import Optional
+from typing import Optional, Dict, List, Tuple
 import Core.botdata as bd
 from interactions import Embed
 from httpx import AsyncClient, ReadTimeout, RequestError, post
@@ -50,8 +50,8 @@ def prev_rec_button() -> Button:
 
 
 async def query_user_statistics(
-    anilist_id: int, media_type: str
-) -> Optional[dict]:
+        anilist_id: int, media_type: str
+) -> Optional[Dict]:
     """
     Queries anilist for user statistics used for weighting/scoring of animanga recommendations
 
@@ -98,9 +98,10 @@ async def query_user_statistics(
         return None
     if response.status_code != 200:
         return None
-    if not response.json()['data']['User']['statistics'][media_type]['count']:
-        return None
     user_data = response.json()['data']['User']
+    if not user_data['statistics'][media_type]['count']:
+        return None
+
     favorites = [fav['id'] for fav in user_data['favourites'][media_type]['nodes']]
     user_data['favourites'][media_type] = favorites
 
@@ -108,13 +109,13 @@ async def query_user_statistics(
 
 
 async def query_media_recs(
-    anilist_id: int, media_type: str, watched_count: int
-) -> Optional[list[dict]]:
+        anilist_id: int, media_type: str, watched_count: int
+) -> Optional[List[Dict]]:
     """
     Queries anilist for user list data used for weighting/scoring of animanga recommendations
 
     Args:
-        anilist_id (str): Anilist user ID to query
+        anilist_id (int): Anilist user ID to query
         media_type (str): Specifies anime or manga statistics
         watched_count (int): Completed entries on user's list
 
@@ -164,26 +165,34 @@ async def query_media_recs(
     """
 
     chunk_size = 100
+    max_concurrent = Semaphore(6)
 
     async def query_list_recommendations(session: AsyncClient, chunk):
-        req_vars = {
-            'userId': anilist_id,
-            'type': media_type.upper(),
-            'statusNotIn': 'PLANNING',
-            'perPage': 8,
-            'sort': 'RATING_DESC',
-            'perChunk': chunk_size,
-            'chunk': chunk,
-        }
-        try:
-            data = await session.post(
-                url='https://graphql.anilist.co',
-                json={'query': query, 'variables': req_vars},
-                timeout=10,
-            )
-        except ReadTimeout:
-            return None
-        return data
+        max_attempts = 3
+        for attempt in range(max_attempts):
+            req_vars = {
+                'userId': anilist_id,
+                'type': media_type.upper(),
+                'statusNotIn': 'PLANNING',
+                'perPage': 8,
+                'sort': 'RATING_DESC',
+                'perChunk': chunk_size,
+                'chunk': chunk,
+            }
+            async with max_concurrent:
+                try:
+                    data = await session.post(
+                        url='https://graphql.anilist.co',
+                        json={'query': query, 'variables': req_vars},
+                        timeout=10,
+                    )
+                    if data.status_code == 200:
+                        return data
+                except ReadTimeout:
+                    pass
+
+            await sleep((1.75 ** attempt) + uniform(0, 1))
+        return None
 
     tasks: list = []
 
@@ -208,17 +217,17 @@ async def query_media_recs(
 
 
 async def fetch_recommendations(
-    anilist_id: int, media_type: str
-) -> tuple[list, dict, list]:
+        anilist_id: int, media_type: str
+) -> Tuple[List, Dict, List]:
     """
     Wrapper function for fetching anilist data for animanga recs
 
     Args:
-        anilist_id (int): Anilist username to query
+        anilist_id (int): Anilist user ID to query
         media_type (str): Specifies anime or manga statistics
 
     Returns:
-        tuple: Tuple containing user list data and user statistics
+        tuple: Tuple containing user list data, user statistics, and favorites
 
     Raises:
         RequestError if either user statistics or list data is empty
@@ -243,22 +252,27 @@ async def fetch_recommendations(
 
 
 def calculate_rec_scores(
-    list_data: list[dict], user_stats: dict, user_favorites: list
-) -> list[MediaRec]:
+        list_data: List[Dict], user_stats: Dict, user_favorites: List[int]
+) -> List[MediaRec]:
     """
     Scoring algorithm for animanga recs
 
     Args:
         list_data (list[dict]): Anilist media list collection data
         user_stats (dict): Anilist user statistics
-        user_favorites (list): List of anilist user favorited media IDs
+        user_favorites (list[int]): List of user favorited media IDs
 
     Returns:
         list[MediaRec]: List of user's recommendations
     """
+    max_popularity = 0
+    global_mean = 65
+    genre_count_weight = 0.16
+    popularity_exp = 1.5
+    global_scale_exp = 0.35
+
     # Obtain max user score, collect watched show info
     max_score = 1
-    max_popularity = 0
     seen_show_ids = []
     for entry in list_data:
         seen_show_ids.append(entry['media']['id'])
@@ -267,17 +281,20 @@ def calculate_rec_scores(
         if entry['media']['popularity'] > max_popularity:
             max_popularity = entry['media']['popularity']
 
+    seen_show_ids = set(seen_show_ids)
+
     # Get user genre scores
     user_genre_scores = {}
     for genre in user_stats['genres']:
         genre_name = genre['genre']
         if not genre['meanScore']:
             user_genre_scores[genre_name] = 0
-        user_genre_scores[genre_name] = (
-                                                genre['meanScore'] - user_stats['meanScore']
-                                        ) / 100 + (genre['count'] - 0.5 * len(seen_show_ids)) / len(
-            seen_show_ids
-        ) * 0.16
+        else:
+            user_genre_scores[genre_name] = (
+                                                    genre['meanScore'] - user_stats['meanScore']
+                                            ) / 100 + (genre['count'] - 0.5 * len(seen_show_ids)) / len(
+                seen_show_ids
+            ) * genre_count_weight
 
     recommendation_scores: dict[int:MediaRec] = {}
     for entry in list_data:
@@ -301,7 +318,7 @@ def calculate_rec_scores(
             if show_rec['mediaRecommendation']['id'] in seen_show_ids:
                 continue
             if not show_rec['mediaRecommendation']['meanScore']:
-                show_rec['mediaRecommendation']['meanScore'] = 65
+                show_rec['mediaRecommendation']['meanScore'] = global_mean
 
             # Filter out shows with prequels that have not been seen yet
             try:
@@ -317,33 +334,37 @@ def calculate_rec_scores(
             except KeyError:
                 pass
 
-            # Scoring
+            # Scoring weights
             node_score_weight = 0 if entry['score'] == 0 else 0.8
-            node_score = node_score_weight * (
-                    entry['score'] / max_score - user_stats['meanScore'] / 100
-            )
             rec_show_score_weight = 1
-            rec_show_score = (
-                    rec_show_score_weight
-                    * (show_rec['mediaRecommendation']['meanScore'] - 65)
-                    / 100
-            )
             rec_pop_factor = (
                     1 - show_rec['mediaRecommendation']['popularity'] / max_popularity
             )
-            rec_pop_factor = rec_pop_factor ** 1.5 if rec_pop_factor > 0 else 0.1
             rec_genre_score_weight = 1.5
+            rec_pop_factor = (
+                rec_pop_factor ** popularity_exp if rec_pop_factor > 0 else 0.1
+            )
+            rec_total_weight = show_rec['rating'] / max_rec_rating
+
+            # Scoring
+            node_score = node_score_weight * (
+                    entry['score'] / max_score - user_stats['meanScore'] / 100
+            )
+            rec_show_score = (
+                    rec_show_score_weight
+                    * (show_rec['mediaRecommendation']['meanScore'] - global_mean)
+                    / 100
+            )
             rec_genre_score = 0
             for genre in show_rec['mediaRecommendation']['genres']:
                 try:
                     rec_genre_score += user_genre_scores[genre] / len(
                         show_rec['mediaRecommendation']['genres']
                     ) ** (1 / 2)
-                except KeyError or ZeroDivisionError:
+                except (KeyError, ZeroDivisionError):
                     continue
                 rec_genre_score *= rec_genre_score_weight
 
-            rec_total_weight = show_rec['rating'] / max_rec_rating
             total_rec_score = (
                     (node_score + rec_show_score + rec_genre_score)
                     * rec_total_weight
@@ -355,10 +376,7 @@ def calculate_rec_scores(
                     MediaRec(
                         media_id=show_rec['mediaRecommendation']['id'],
                         title=show_rec['mediaRecommendation']['title']['romaji'],
-                        genres=[
-                            genre.lower()
-                            for genre in show_rec['mediaRecommendation']['genres']
-                        ],
+                        genres=show_rec['mediaRecommendation']['genres'],
                         cover_url=show_rec['mediaRecommendation']['coverImage'][
                             'large'
                         ],
@@ -381,21 +399,21 @@ def calculate_rec_scores(
     # Normalize scores and apply filter for logical percentages
     max_score = recommendation_scores[0].score
     for rec in recommendation_scores:
-        rec.score = (rec.score / max_score) ** 0.35 * 100
+        rec.score = (rec.score / max_score) ** global_scale_exp * 100
 
     return recommendation_scores
 
 
 async def check_recommendation(
-    anilist_id: int,
-    media_type: str,
-    force_update: bool = False,
+        anilist_id: int,
+        media_type: str,
+        force_update: bool = False,
 ) -> None:
     """
     Check if recommendations exist in cache and are up to date, and fetch new data if not cached.
 
     Args:
-        anilist_id (int): Anilist username to recommend for
+        anilist_id (int): Anilist user ID to query
         media_type (str): Anilist user statistics
         force_update (bool): If true, will always fetch new data from anilist instead of using cache
     """
@@ -406,15 +424,11 @@ async def check_recommendation(
     # Use cached data unless cached data does not exist or is outdated
     try:
         time_delta = (
-            datetime.now()
-            - datetime.strptime(
-                known_recs[anilist_id]['date'], '%Y/%m/%d %H:%M:%S'
-            )
+                datetime.now() - known_recs[anilist_id]['date']
         ).total_seconds()
     except KeyError:
         time_delta = 0
     if anilist_id not in known_recs or force_update or time_delta > 345600:
-
         list_data, user_stats, user_favorites = await fetch_recommendations(
             anilist_id=anilist_id,
             media_type=media_type,
@@ -424,17 +438,11 @@ async def check_recommendation(
             user_stats=user_stats,
             user_favorites=user_favorites,
         )
+        known_recs[anilist_id] = {
+            'date': datetime.now(),
+            'recs': recommendation_scores,
+        }
 
-        if media_type.lower() == 'manga':
-            bd.known_manga_recs[anilist_id] = {
-                'date': datetime.now().strftime('%Y/%m/%d %H:%M:%S'),
-                'recs': recommendation_scores,
-            }
-        else:
-            bd.known_anime_recs[anilist_id] = {
-                'date': datetime.now().strftime('%Y/%m/%d %H:%M:%S'),
-                'recs': recommendation_scores,
-            }
     return None
 
 
