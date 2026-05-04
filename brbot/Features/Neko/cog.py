e0c60a152485fe9b3bb6a6249a4a6069f5b4a8d1
MrGarlic1 <ben@samans.com>
2026-05-04 18:36:20 -0400

    feat: limit command

--- brbot/Features/Neko/cog.py
+++ brbot/Features/Neko/cog.py
@@ -8,19 +8,29 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+## Nuke this entire class later; temp feature
 class NekoCog(commands.GroupCog, name="neko"):
     def __init__(self, bot: BrBot):
         # self.neko_service = NekoService()
         self.bot = bot
+        self.last_userid_by_guild: dict[int, int] = {}
 
     @app_commands.command(
         name="pic",
         description="Send a picture of a random catgirl",
     )
     async def neko(self, ctx: Interaction):
-        embed = Embed(title="🖼️🐱Neko")
-        embed.set_image(url=choice(neko_urls))
-        embed.set_footer(text="Provided by Mr.Garlic")
+        if ctx.user.id == self.last_userid_by_guild.get(ctx.guild.id):
+            embed = Embed(title="⛔⛔ NOT Neko")
+            embed.set_image(url="https://i.imgur.com/YD1cOub.png")
+            embed.set_footer(
+                text="NOT Provided by Mr.Garlic. Wait your turn next time."
+            )
+        else:
+            embed = Embed(title="🖼️🐱Neko")
+            embed.set_image(url=choice(neko_urls))
+            self.last_userid_by_guild[ctx.guild.id] = ctx.user.id
+            embed.set_footer(text="Provided by Mr.Garlic")
 
         await ctx.response.send_message(embed=embed)
 
