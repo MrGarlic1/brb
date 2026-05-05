f2ac228ed2863da3b0e9a737f213504973bb9a48
MrGarlic1 <ben@samans.com>
2026-05-05 02:46:41 -0400

    fix: improve user/member lookup functions

--- brbot/Shared/Members/repository.py
+++ brbot/Shared/Members/repository.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy import select
+from sqlalchemy import select, tuple_
 from brbot.db.models import Member
 
 
@@ -12,20 +12,54 @@ async def get_or_create_member(
         .where(Member.user_id == user_id)
         .where(Member.guild_id == guild_id)
     )
-
-    result = await session.execute(stmt)
-    member: Member = result.scalar_one_or_none()
-    if member:
-        return member
-
-    member = Member(user_id=user_id, guild_id=guild_id)
-    session.add(member)
-
-    try:
-        await session.commit()
-        return member
-    except IntegrityError:
-        await session.rollback()
+    async with session.begin_nested():
         result = await session.execute(stmt)
-        user = result.scalar_one()
-        return user
+        member: Member = result.scalar_one_or_none()
+        if member:
+            return member
+
+        member = Member(user_id=user_id, guild_id=guild_id)
+        session.add(member)
+
+        try:
+            await session.flush()
+            return member
+        except IntegrityError:
+            pass
+
+        result = await session.execute(stmt)
+        member = result.scalar_one_or_none()
+        if member is None:
+            raise RuntimeError(
+                f"Member with user ID {user_id} and guild id {guild_id} is unexpectedly not found."
+            )
+        return member
+
+
+async def get_or_create_members(
+    user_ids: list[int],
+    guild_id_values: list[int] | int,
+    session: AsyncSession,
+) -> list[Member]:
+    if isinstance(guild_id_values, int):
+        guild_id_values = [guild_id_values] * len(user_ids)
+
+    if len(user_ids) != len(guild_id_values):
+        raise ValueError("User IDs and guild IDs must have the same number of entries.")
+
+    id_pairs = list(zip(user_ids, guild_id_values))
+
+    # Preload existing members
+    stmt = select(Member).where(tuple_(Member.user_id, Member.guild_id).in_(id_pairs))
+    result = await session.execute(stmt)
+    existing = {(m.user_id, m.guild_id): m for m in result.scalars()}
+
+    members: list[Member] = []
+
+    for user_id, guild_id in id_pairs:
+        member = existing.get((user_id, guild_id))
+        if member is None:
+            member = await get_or_create_member(user_id, guild_id, session)
+        members.append(member)
+
+    return members
--- brbot/Shared/Users/repository.py
+++ brbot/Shared/Users/repository.py
@@ -2,6 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy import select
 from brbot.db.models import User
+from typing import Sequence
 
 
 async def get_or_create_user(
@@ -9,22 +10,50 @@ async def get_or_create_user(
 ) -> User:
     stmt = select(User).where(User.user_id == user_id)
 
-    result = await session.execute(stmt)
-    user: User = result.scalar_one_or_none()
-    if user is not None and user.name == username:
-        return user
-
-    if user is not None:
-        user.name = username
-    else:
-        user = User(user_id=user_id, name=username)
-        session.add(user)
-
-    try:
-        await session.commit()
-        return user
-    except IntegrityError:
-        await session.rollback()
+    async with session.begin_nested():
         result = await session.execute(stmt)
-        user = result.scalar_one()
+        user: User = result.scalar_one_or_none()
+        if user is not None and user.name == username:
+            return user
+
+        if user is not None:
+            user.name = username
+        else:
+            user = User(user_id=user_id, name=username)
+            session.add(user)
+
+        try:
+            await session.flush()
+            return user
+        except IntegrityError:
+            pass
+
+        result = await session.execute(stmt)
+        user = result.scalar_one_or_none()
+        if user is None:
+            raise RuntimeError(f"User {user_id} is unexpectedly not found.")
         return user
+
+
+async def get_or_create_users(
+    user_ids: list[int], usernames: list[str], session: AsyncSession
+) -> list[User]:
+    if len(user_ids) != len(usernames):
+        raise ValueError("Users and usernames must have the same length")
+
+    user_info = dict(zip(user_ids, usernames))
+
+    stmt = select(User).where(User.user_id.in_(user_ids))
+
+    result = await session.execute(stmt)
+    existing: Sequence[User] = result.scalars().all()
+    existing_users = {u.user_id: u for u in existing}
+
+    users: list[User] = []
+    for user_id in user_ids:
+        user = existing_users.get(user_id, None)
+        if user is None:
+            user = await get_or_create_user(user_id, user_info[user_id], session)
+        users.append(user)
+
+    return users
