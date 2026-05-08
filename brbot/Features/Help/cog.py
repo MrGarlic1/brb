c0cc970824b21d6118cf4f6509cabcc8d0e4a68c
MrGarlic1 <ben@samans.com>
2026-05-08 14:50:16 -0400

    feat: add about command

--- brbot/Features/Help/cog.py
+++ brbot/Features/Help/cog.py
@@ -1,6 +1,7 @@
 import brbot.Features.Help.data as hp
 from brbot.Core.bot import BrBot
-from discord import app_commands, Interaction
+from brbot.Core.botdata import bot_avatar_url
+from discord import app_commands, Interaction, Embed
 from discord.ext import commands
 
 
@@ -14,6 +15,24 @@ class HelpCog(commands.Cog):
     async def help(self, ctx: Interaction):
         embed = hp.gen_help_embed(category="general")
         await ctx.response.send_message(embed=embed, view=hp.HelpView())
+
+    @app_commands.command(name="about", description="Information about the bot")
+    async def about(self, ctx: Interaction):
+        embed = Embed(title="Response Bot", description="Statistics")
+        embed.set_author(
+            name=f"Requested by {ctx.user.mention}", icon_url=ctx.user.avatar.url
+        )
+        embed.add_field(
+            name="\u200b", value=f"Currently in **{len(self.bot.guilds)}** guilds"
+        )
+        embed.add_field(
+            name="\u200b", value=f"Latency: {(1000 * self.bot.latency):.1f} ms"
+        )
+        embed.set_thumbnail(url=bot_avatar_url)
+        embed.set_footer(
+            text="Response Bot is open source. View the source code at https://github.com/MrGarlic1/brb"
+        )
+        await ctx.response.send_message(embed=embed)
 
 
 async def setup(bot: BrBot):
